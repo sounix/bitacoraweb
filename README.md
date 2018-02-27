59e1cf665550105e5a128efb268030e5e4ffcc63
sounix <sounix@gmail.com>
2018-02-27 10:50:11 -0600

    Carga de Proyecto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bitacoraweb
 Proyecto Para SPA2018 - BItacoraWeb
+
+> By Aldrin Gonzalez Cancino <sounix@gmail.com>
---
+++ test.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const libreriaEAN13 = require('./index')
+
+const barcode = '7501011123588'
+console.log(libreriaEAN13.stringDebug(barcode))
